46d99cd6570e4c3a353f9da7ff4818de84c2f4dd
Berend Zwartsenberg <berend@zwartsenberg.nl>
2020-05-06 08:50:56 -0700

    Swap around code and clean up

--- integrated_loss.py
+++ integrated_loss.py
@@ -8,7 +8,6 @@ Created on Mon May  4 17:46:18 2020
 
 import torch.nn.functional as F
 import torch
-import torch.optim as optim
 import itertools
 
 def get_binaries(num_bernoullis):
@@ -21,79 +20,6 @@ def get_binaries(num_bernoullis):
         array (1, num_bernoullis, 2^num_bernoullis)
     """
     return torch.tensor(list(itertools.product([0, 1], repeat = num_bernoullis))).unsqueeze(1).float()
-
-def expectation_loss(f,x,logit_q,logit_q_prior,logit_p_x_given_b, kl_rec_ratio):
-    """
-    Args:
-        f: a function that takes x, b, q, q_prior, logit_p as argument, x is input,
-            b is bernoulli vector, q is q(b|x), q_prior is the prior, logit_p is rec. probability logit
-            shape: see "loss_arg"
-        x: input data
-            shape: (batch,in_shape)
-        logit_q: logit probability distribution of n bernoulli variables
-            shape: (batch, num_bernoullis)
-        logit_q_prior: logit probability distribution of n bernoulli variables for the prior
-            shape: (1, num_bernoullis)
-        logit_p_x_given_b: a function that returns logit_p(x|b)
-        kl_rec_ratio: a number that specifies the ratio of kl_loss to rec_loss. Note that both are taken as a mean, not a sum.
-                      i.e., for normal 28*28 input and 10 binary latents, use 10/(28*28)
-            
-            """
-    binary_vecs = get_binaries(logit_q.shape[1]) #size of bernoulli's
-    total_kl_loss = torch.zeros(logit_q.shape[0]) #size of batch
-    total_rec_loss = torch.zeros(logit_q.shape[0]) 
-    total_weight = torch.zeros(logit_q.shape[0])
-
-    #first, need to get all the logit_p's for binary vecs. Therefore, need all binary vecs in 
-    #dim[0], as (num_binary_vecs, num_binaries)
-    logit_p = logit_p_x_given_b(binary_vecs.squeeze())
-    #above gives logit_p as (num_binaries,in_size), but need (1,num_binaries,num_binary_vecs)
-    logit_p = logit_p.transpose(1,0).unsqueeze(0)
- 
-    #same for binary_vecs:
-    binary_vecs = binary_vecs.squeeze().transpose(1,0).unsqueeze(0)
-
-    #add dimension for binary_vecs:
-    logit_q = logit_q.unsqueeze(2)
-    logit_q_prior = logit_q_prior.unsqueeze(2)
-    x = x.unsqueeze(2)
-
-    
-    #expanding for KL loss:, because crossentropy function does not broadcast
-    logit_q = logit_q.expand(-1,-1,binary_vecs.shape[2])
-    binary_vecs = binary_vecs.expand(*logit_q.shape)
-    logit_q_prior = logit_q_prior.expand(*logit_q.shape)
-    
-    #expanding for rec. loss:
-    logit_p = logit_p.expand(x.shape[0],-1,-1)
-    x = x.expand(-1,-1,logit_p.shape[2])    
-    
-    #the log_prob, this is equivalent to: prob = q*binary_vecs + (1-binary_vecs)*(1-q), but with logits
-    log_prob = -F.binary_cross_entropy_with_logits(logit_q, binary_vecs, reduction='none')
-
-    # and sum all the binary terms:
-    log_prob = torch.sum(log_prob, dim=1, keepdims=False)
-    
-    # f should return rec_loss and kl_loss as:
-    #(batch, num_binary_vecs)
-    rec_loss, kl_loss = f(x, binary_vecs, logit_q, logit_q_prior, logit_p)
-
-    #could sum to the logarithm if getting under/overflow issues
-    total_rec_loss = (rec_loss*torch.exp(log_prob)).sum(axis=1)
-    total_kl_loss = (kl_loss*torch.exp(log_prob)).sum(axis=1)
-
-    #sanity check
-    tol = 1e-5
-    total_weight = torch.exp(log_prob).sum(axis=(1))
-    if torch.any(torch.abs(total_weight - torch.ones_like(total_weight))>tol):
-        print('Probs dont sum to one')
-        print(total_weight)
-        
-    total_loss =  total_rec_loss + total_kl_loss*kl_rec_ratio
-    return total_loss.mean(), total_rec_loss.mean(), total_kl_loss.mean()
-
-
-
 
 #losses:
 def neg_DKL(logit_q, logit_q_prior, b):
@@ -154,3 +80,78 @@ def loss_arg(x, b, logit_q, logit_q_prior, logit_p):
     rec_loss = log_likelihood(logit_p,x)
     kl_loss = neg_DKL(logit_q, logit_q_prior, b) 
     return rec_loss, kl_loss
+
+
+def expectation_loss(x,logit_q,logit_q_prior,logit_p_x_given_b, kl_rec_ratio, f=loss_arg):
+    """
+    Args:
+        x: input data
+            shape: (batch,in_shape)
+        logit_q: logit probability distribution of n bernoulli variables
+            shape: (batch, num_bernoullis)
+        logit_q_prior: logit probability distribution of n bernoulli variables for the prior
+            shape: (1, num_bernoullis)
+        logit_p_x_given_b: a function that returns logit_p(x|b)
+        kl_rec_ratio: a number that specifies the ratio of kl_loss to rec_loss. Note that both are taken as a mean, not a sum.
+                      i.e., for normal 28*28 input and 10 binary latents, use 10/(28*28)
+        f: a function that takes x, b, q, q_prior, logit_p as argument, x is input,
+            b is bernoulli vector, q is q(b|x), q_prior is the prior, logit_p is rec. probability logit
+            shape: see "loss_arg"
+            
+            """
+    binary_vecs = get_binaries(logit_q.shape[1]) #size of bernoulli's
+    total_kl_loss = torch.zeros(logit_q.shape[0]) #size of batch
+    total_rec_loss = torch.zeros(logit_q.shape[0]) 
+    total_weight = torch.zeros(logit_q.shape[0])
+
+    #first, need to get all the logit_p's for binary vecs. Therefore, need all binary vecs in 
+    #dim[0], as (num_binary_vecs, num_binaries)
+    logit_p = logit_p_x_given_b(binary_vecs.squeeze())
+    #above gives logit_p as (num_binaries,in_size), but need (1,num_binaries,num_binary_vecs)
+    logit_p = logit_p.transpose(1,0).unsqueeze(0)
+ 
+    #same for binary_vecs:
+    binary_vecs = binary_vecs.squeeze().transpose(1,0).unsqueeze(0)
+
+    #add dimension for binary_vecs:
+    logit_q = logit_q.unsqueeze(2)
+    logit_q_prior = logit_q_prior.unsqueeze(2)
+    x = x.unsqueeze(2)
+
+    
+    #expanding for KL loss:, because crossentropy function does not broadcast
+    logit_q = logit_q.expand(-1,-1,binary_vecs.shape[2])
+    binary_vecs = binary_vecs.expand(*logit_q.shape)
+    logit_q_prior = logit_q_prior.expand(*logit_q.shape)
+    
+    #expanding for rec. loss:
+    logit_p = logit_p.expand(x.shape[0],-1,-1)
+    x = x.expand(-1,-1,logit_p.shape[2])    
+    
+    #the log_prob, this is equivalent to: prob = q*binary_vecs + (1-binary_vecs)*(1-q), but with logits
+    log_prob = -F.binary_cross_entropy_with_logits(logit_q, binary_vecs, reduction='none')
+
+    # and sum all the binary terms:
+    log_prob = torch.sum(log_prob, dim=1, keepdims=False)
+    
+    # f should return rec_loss and kl_loss as:
+    #(batch, num_binary_vecs)
+    rec_loss, kl_loss = f(x, binary_vecs, logit_q, logit_q_prior, logit_p)
+
+    #could sum to the logarithm if getting under/overflow issues
+    total_rec_loss = (rec_loss*torch.exp(log_prob)).sum(axis=1)
+    total_kl_loss = (kl_loss*torch.exp(log_prob)).sum(axis=1)
+
+    #sanity check
+    tol = 1e-5
+    total_weight = torch.exp(log_prob).sum(axis=(1))
+    if torch.any(torch.abs(total_weight - torch.ones_like(total_weight))>tol):
+        print('Probs dont sum to one')
+        print(total_weight)
+        
+    total_loss =  total_rec_loss + total_kl_loss*kl_rec_ratio
+    return total_loss.mean(), total_rec_loss.mean(), total_kl_loss.mean()
+
+
+
+
